f5dbcddb35909607e67f64e70f487cdb87365f02
Fhevus <98560000+Fhevus@users.noreply.github.com>
2023-07-27 05:12:15 +0000

    set homepage for github paages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Emeka-Nwafor Favour",
+  title: "Data Analyst",
+  email: "favouremeka100@gmail.com",
+  gitHub: "https://github.com/Fhevus",
+  twitter: "https://twitter.com/Favouremeka28",
 };
 
 const primaryColor = "#4E567E";
